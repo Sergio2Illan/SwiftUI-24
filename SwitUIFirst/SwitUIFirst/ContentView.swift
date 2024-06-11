beeade0d67729f03bd7b81ae9012a193bb027245
Sergio2Illan <illan2sergio@gmail.com>
2024-06-11 23:54:39 +0200

    Add some views

--- SwitUIFirst/SwitUIFirst/ContentView.swift
+++ SwitUIFirst/SwitUIFirst/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!¿").font(.title).bold()
+        ZStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Siguiendo").font(.largeTitle).bold()
+                Text("Canales recomendados").foregroundColor(.gray).textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                HStack {
+                    Rectangle().foregroundColor(.blue)
+                        .frame(width: 118, height: 70)
+                    VStack(alignment: .leading, spacing: 10 ){
+                        HStack {
+                            Circle().frame(width: 16, height: 16)
+                                .foregroundColor(.blue)
+                            Text("Username").font(.headline)
+                        }
+                        Text("Streaming de programación...").foregroundColor(.gray)
+                        Text("Solo Hablando").foregroundColor(.gray)
+                        
+                    }
+                }
+            }
+            Text("Suscribiros a mi canal")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.red)
+                .underline()
+                .background(Color.black)
+                .rotationEffect(.degrees(-20))
         }
-        .padding()
+        
     }
 }
 
